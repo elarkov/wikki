a5a94d5eec6a373c4fc2b0feb520bcb119219780
Evgeniy Larkov <elarkov@inbox.ru>
2023-08-01 19:59:51 +0700

    Добавил блок с ссылками на приложение

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 import * as functions from "./modules/functions.js";
 import * as sideMenu from "./modules/side-menu.js";
 import * as phoneinput from "./modules/phone-validate.js";
+import * as ajax from "./modules/ajax.js";
 //import * as slider from "./modules/slider.js";
 //import * as popup from "./modules/popup.js";
 //import * as doNotificationToast from "./modules/notification-toast.js";
@@ -12,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function () { //dom is ready
   functions.isWebp();
   sideMenu.sideMenu();
   phoneinput.phoneInput();
+  ajax.ajaxSend();
   //slider.renderSlider();
   //popup.renderPopup();
   //doNotificationToast.doNotificationToast();
---
+++ src/js/modules/ajax.js
@@ -0,0 +1,43 @@
+export function ajaxSend() {
+  // Находим все формы на странице
+  var forms = document.querySelectorAll('form');
+
+  // Перебираем все формы
+  for (var i = 0; i < forms.length; i++) {
+    var form = forms[i];
+
+    // Добавляем обработчик события отправки формы
+    form.addEventListener('submit', function (event) {
+      // Отменяем стандартное действие отправки формы
+      event.preventDefault();
+
+      // Получаем данные текущей формы
+      var name = this.elements.client.value;
+      var phone = this.elements.phone.value;
+
+      // Проверяем, что номер телефона соответствует формату российского номера
+      // if (!validatePhone(phone)) {
+      //   alert('Пожалуйста, введите корректный номер телефона.');
+      //   return;
+      //}
+
+      // Создаем объект FormData для отправки данных на сервер
+      var formData = new FormData();
+      formData.append('client', name);
+      formData.append('phone', phone);
+
+      // Создаем AJAX-запрос
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', this.action, true);
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+          // Переходим на другую страницу после успешной отправки данных
+          window.location.href = 'success.html';
+        }
+      };
+
+      // Отправляем данные на сервер
+      xhr.send(formData);
+    });
+  }
+}
